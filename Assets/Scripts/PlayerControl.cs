e3fd1cc30cd9af7c49b69e8e73d7d7066b9227bb
Nightmarez29 <Maddexkern@Gmail.com>
2024-11-18 10:28:15 -0800

    AsteroidsCloneDay1

---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    [Header("Player Movement")]
+    private float horizontalInput;
+    private float forwordInput;
+    public float turnSpeed;
+    public float moveSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwordInput = Input.GetAxis("Vertical");
+
+        transform.Translate(Vector3.forward * Time.deltaTime * moveSpeed * forwordInput);
+        transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
+    }
+}
