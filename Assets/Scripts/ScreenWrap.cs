0ca1f519070ac308f646de8e25507a9207ddc41e
Nightmarez29 <Maddexkern@Gmail.com>
2024-11-19 08:57:08 -0800

    Screen Wrapping

---
+++ Assets/Scripts/ScreenWrap.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenWrap : MonoBehaviour
+{
+    [Header("Limits")]
+    public float xMin;
+    public float xMax;
+    public float zMin;
+    public float zMax;
+
+   
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.position.x > xMax)
+        {
+            transform.position = new Vector3(
+                xMin,
+                transform.position.y,
+                transform.position.z
+                );
+        }
+
+        if(transform.position.x < xMin)
+        {
+            transform.position = new Vector3(
+                xMax,
+                transform.position.y,
+                transform.position.z
+                );
+        }
+
+        if(transform.position.x > zMax)
+        {
+            transform.position = new Vector3(
+               transform.position.x,
+               transform.position.y,
+               zMin
+               );
+        }
+
+        if(transform.position.x > zMin)
+        {
+            transform.position = new Vector3(
+                transform.position.x,
+                transform.position.y,
+                zMax
+                );
+        }
+    }
+}
